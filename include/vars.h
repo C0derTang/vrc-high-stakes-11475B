9dac374bfe9a0392d78833b7f2ba0557fd1fdd9f
Option-3 <victorpeel3@icloud.com>
2024-09-07 19:27:59 -0700

    thanks

--- include/vars.h
+++ include/vars.h
@@ -1,37 +1,5 @@
 //hi
 
-//universal value for pi -- truncated to fifth decimal for consistency
-double apple = 3.14159;
-
-//wheel sizes inches -- we'll probaby forget soooo
-double FourOmni = 4.125;
-double TwoOmni = 2.75;
-
-//tracking wheel vars -- will be fun!!
-//starting with defining robot's starting vectors
-//think a reliable reference for direction is positive side at 0 degrees
-double posy = 0;
-double posx = 0;
-double direction = 0;
-
-//perpendicular distance from center of rotation to tracking wheels -- will update later
-double rightTrackDist = 7.375;
-double leftTrackDist = 7.375;
-double midTrackDist = 0.375;
-
-//current encoder values
-int leftEncode;
-int rightEncode;
-int midEncode;
-
-//dist traveled per wheel; f/- b/+  l/- r/+
-double leftDisplace;
-double rightDisplace;
-double midDisplace;
-
-//array of doubles noting current tracking information from the tracking algorithm, {x,  y, 𝞱}
-double trackingInfo[3] = {0, 0, 0};
-
 //PID variable tuning (will take fucking forever)
 //straight tuning
 double kP = 0.5;
@@ -42,30 +10,52 @@ double tkP = 0.5;
 double tkI = 0.2;
 double tkD = 0.0;
 
+
+
 // Autonomous settings
-int desiredValue = 200;
-int desiredTurnValue = 0;
+double driveDist = 0;
+double desiredValue;
+
+double posx;
+double posy;
+double heading;
+
+double vectors[3] = {0, 0, 0}; //x, y, and heading
+
+double prevL = 0;
+double prevR = 0;
+double prevM = 0;
+double curDeg = 0;
+double targetDeg = 0;
 
 //lateral PID vals
-int error = 0;
-int prevError = 0;
-int derivative;
-int totalError = 0; //integral
+double error = 0;
+double prevError = 0;
+double derivative;
+double totalError = 0; //integral
 
 //turn PID vals
-int turnError = 0;
-int turnPrevError = 0;
-int turnDerivative;
-int turnTotalError = 0; //integral
-
-//maintains sensor data when driver control starts
-bool resetDriveSensors = false;
+double turnError = 0;
+double turnPrevError = 0;
+double turnDerivative;
+double turnTotalError = 0; //integral
+double desiredTurnValue;
 
 // Important settings!
 bool enableDrivePID = true;
+bool enableTurnPID = true;
+bool resetDriveSensors = true;
 bool noBitches = true; //conditional for driver control loop -- the code doesn't lie
 double turnImportance = 0.5;
 
 //transmission settings
 bool transmission = false;
 bool tlatch = false;
+
+//constants
+const double PI = 3.14159265;
+const double lWheelDist = 7.375;
+const double rWheelDist = 7.375;
+const double mWheelDist = 0.375;
+const double TwoOmni = 2.75;
+const double FourOmni = 4;
