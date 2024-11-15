d57ce8cd40524e636e8f6e1b72c452b16ca0e211
C0dertang <tangchristopher111@gmail.com>
2024-11-14 16:22:26 -0800

    odometry work

--- include/vars.h
+++ include/vars.h
@@ -15,64 +15,19 @@ double lpower = 0;
 double rpower = 0;
 double driveDist = 0;
 
-double temprot = 0;
-
 double prevL = 0;
 double prevR = 0;
+double prevS = 0;
 double curDeg = 0;
-double targetDeg = 0;
 
-//lateral PID vals for left and right
-double lerror = 0;
-double lprevError = 0;
-double lderivative=0;
-double ltotalError = 0; //integral
-double rerror = 0;
-double rprevError = 0;
-double rderivative=0;
-double rtotalError = 0; //integral
-
-//turn PID vals
-double turnError = 0;
-double turnPrevError = 0;
-double turnDerivative=0;
-double turnTotalError = 0; //integral
-
-// Important settings!
-bool enableDrivePID = true;
-bool enableTurnPID = true;
-bool noBitches = true; //conditional for driver control loop -- the code doesn't lie
 double turnImportance = 0.5;
 
-//transmission settings
-
-bool clamped = false;
+double xPos = 0;
+double yPos = 0;
 
 //constants
 const double PI = 3.14159265;
+const double wheelDiameter = 4.125;
 const double lWheelDist = 7.375;
 const double rWheelDist = 7.375;
-
-// universal object for toggle states, makes variable management easier
-struct Toggle{
-    bool state = false;
-    bool latch = false;
-
-    void check(bool cond){
-      if (cond){
-        if (!latch){
-          state  = !state;
-          latch = true;
-        }
-        }else{
-        latch = false;
-        }
-    }
-};
-
-Toggle tlatch;
-  Toggle clatch;
-  Toggle a1latch;
-  Toggle a2latch;
-
-double speed = 12.0;
+const double sWheelDist = 7.375;
--- src/main.cpp
+++ src/main.cpp
@@ -22,42 +22,15 @@ competition Competition;
 // define your global instances of motors and other devices here
 brain Thinky;
 
-motor lm1(1, false);
-motor lm2(2, false);
-motor lm3(0, false);
-motor rm1(7, true);
-motor rm2(8, true);
-motor rm3(9, true);
+motor lm(0, false);
+motor rm1(5, true);
 
-digital_out armp1 = digital_out(Thinky.ThreeWirePort.A);
-digital_out armp2 = digital_out(Thinky.ThreeWirePort.C);
-
-inertial whee(10);
-
-
-limit  armreset  = limit(Thinky.ThreeWirePort.B);
-
-
-motor_group leftMotor1(lm1,lm2);
-motor_group rightMotor1(rm1,rm2);
-
-motor_group leftMotor(lm1, lm2, lm3);
-motor_group rightMotor(rm1, rm2, rm3);
-
-motor intake1(5,ratio18_1);
-motor intake2(6, ratio18_1, true);
-motor intake3(3, ratio18_1, true);
-motor_group intake(intake1, intake2, intake3);
-
-
-digital_out clamp = digital_out(Thinky.ThreeWirePort.D);
 
 controller sticks;
 
-encoder lquad = encoder(Thinky.ThreeWirePort.G);
-encoder rquad = encoder(Thinky.ThreeWirePort.E);
-//encoder bquad = encoder(Thinky.ThreeWirePort.C);
-
+encoder lquad = encoder(Thinky.ThreeWirePort.A);
+encoder rquad = encoder(Thinky.ThreeWirePort.B);
+encoder squad = encoder(Thinky.ThreeWirePort.C);
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -65,37 +38,24 @@ encoder rquad = encoder(Thinky.ThreeWirePort.E);
 /*                                                                           */
 /*---------------------------------------------------------------------------*/
 
-
-
-
 void reset(){
   lquad.resetRotation();
   rquad.resetRotation();
-  whee.setHeading(0, degrees);
-  curDeg=0;
-}
-void dreset(){
-  temprot = lquad.position(degrees);
-  driveDist=0;
-
+  bquad.resetRotation();
 }
 
-void pre_auton(void) { // in da stripped club, straiht up jorkin' it. and  by it
+void pre_auton(void) {
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, bathroom break etc.
-  leftMotor.setStopping(coast);
-  rightMotor.setStopping(coast);
-  //arm.setStopping(hold);
-  //arm.setMaxTorque(100, percent);
-  //arm.setVelocity(100, percent);
-  intake.setVelocity(100, percent);
+  lm.setStopping(coast);
+  rm.setStopping(coast);
 
   
 }
 
 // Unit Conversions
 double inchtodegrees(double val){
-  double rotations = val/(4.125*PI);
+  double rotations = val/(wheelDiameter*PI);
   double degrees = rotations*360;
   return degrees;
 }
@@ -109,9 +69,23 @@ double radtodegrees(double val){
 int odometry(){
   //very rudimentary, just degree tracking right now
   while(true){
-    curDeg += ((lquad.position(degrees)-prevL) - (rquad.position(degrees)-prevR)) / (lWheelDist + rWheelDist);
+    double deltaL = lquad.position(degrees)-prevL;
+    double deltaR = rquad.position(degrees)-prevR;
+    double deltaS = squad.position(degrees)-prevS;
+
+    double deltaT = (deltaL - deltaR) / (lWheelDist + rWheelDist);
+
+    curDeg += deltaT;
+
+    double tx = 2 * sin(degreestorad(deltaT)/2) * (deltaS/deltaT + sWheelDist);
+    double ty = 2 * sin(degreestorad(deltaT)/2) * (deltaR/deltaT + rWheelDist);
+
     prevL = lquad.position(degrees);
     prevR = rquad.position(degrees);
+    prevS = squad.position(degrees);
+
+    
+
 
     sticks.Screen.clearLine(6);
     sticks.Screen.setCursor(6,0);
@@ -119,63 +93,6 @@ int odometry(){
     
 
     task::sleep(10);
-  }
-  return 1;
-}
-
-int headingPID() {
-  while(enableTurnPID) {
-    // Heading correction logic
-    double headingError = (curDeg)-targetDeg;
-        
-    // Basic PID for heading correction
-    turnTotalError += headingError;
-    if (abs(headingError) <.01 || abs(headingError) > 20) turnTotalError = 0;
-
-    double turnDerivative = headingError - turnPrevError;
-    turnPrevError = headingError;
-
-    // Adjust heading with PID terms
-    double turnPower = headingError * tkP + turnTotalError * tkI + turnDerivative * tkD;
-
-    // Clamp the turn power
-    if (turnPower < -12.0) turnPower = -12.0;
-    if (turnPower > 12.0) turnPower = 12.0;
-
-    // Use turn power to correct heading while driving
-    leftMotor.spin(forward,(lpower - turnPower), voltageUnits::volt);
-    rightMotor.spin(forward, (lpower + turnPower), voltageUnits::volt);
-    
-    
-    task::sleep(5);
-  }
-  return 1;
-}
-
-int ldrivePID(){
-  while(true){
-    if (!enableDrivePID){
-      task::sleep(20);
-      continue;
-    }
-    double lPos = temprot-lquad.position(degrees);
-    
-    lerror = lPos-inchtodegrees(driveDist);
-
-    ltotalError += lerror;
-    if(abs(lerror)<.01 || abs(lerror) > 20) ltotalError=0;
-
-    lderivative = lerror-lprevError;
-    lprevError=lerror;
-
-    // Calculate drive power (forward movement)
-    lpower = lerror * kP + ltotalError * kI + lderivative * kD;
-
-    if (lpower < -speed) lpower = -speed;
-    if (lpower > speed) lpower = speed;
-    
-
-    task::sleep(20);
   }
   return 1;
 }
@@ -188,60 +105,7 @@ int ldrivePID(){
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  reset();
-  dreset();
-  curDeg=0;
   
-  task odom(odometry);
-  task ldpid(ldrivePID);
-  task hpid(headingPID);
-
-    dreset();
-    speed=10.0;
-    driveDist=-11.5;
-    wait(1, seconds);
-    enableDrivePID=false;
-    targetDeg=-30;
-    wait(.8,seconds);
-    dreset();
-    enableDrivePID=true;
-    speed=5.0;
-    driveDist=-12;
-    wait(1.2,seconds);
-    clamp.set(true);
-    wait(.4,seconds);
-    
-    intake.spin(reverse);
-    wait(1,seconds);
-    speed=10.0;
-    driveDist = 20;
-  
-    wait(1,seconds);
-    enableDrivePID=false;
-    speed=5.0;
-    targetDeg = -90;
-    wait(1,seconds);
-    
-    dreset();
-    enableDrivePID=true;
-    speed=5.0;
-    driveDist = 30;
-    wait(1.5,seconds);
-    driveDist=18;
-    wait(1,sec);
-    enableDrivePID=false;
-    targetDeg=-180;
-    wait(.6,sec);
-    dreset();
-    enableDrivePID=true;
-    driveDist=17;
-    wait(2,sec);
-    driveDist=14;
-   wait(1,sec);
-    enableDrivePID=false;
-
-ldpid.stop();
-hpid.stop();
 }
 
 /*---------------------------------------------------------------------------*/
@@ -252,57 +116,18 @@ hpid.stop();
 
 void usercontrol(void) {
   reset();
-  enableDrivePID = false;
-  enableTurnPID=false;
-  //arm.setPosition(0, turns);
 
-  turnImportance = 0.5;
-
-  while (noBitches) {
+  while (true) {
     double turnVal = sticks.Axis1.position(percent);
     double fwdVal = sticks.Axis3.position(percent);
     // volts gives more power, apparently
     double turnVolts = turnVal * 0.12;
     double fwdVolts = fwdVal * 0.12 * (1-(abs(turnVolts/12.0)) * turnImportance);
-    if (tlatch.state){
-      leftMotor1.spin(forward, fwdVolts + turnVolts, voltageUnits::volt);
-      rightMotor1.spin(forward, fwdVolts - turnVolts, voltageUnits::volt);
-    }else{
-      leftMotor.spin(forward, fwdVolts + turnVolts, voltageUnits::volt);
-      rightMotor.spin(forward, fwdVolts - turnVolts, voltageUnits::volt);
-    }
-    clamp.set(clatch.state);
-    armp1.set(a1latch.state);
-    armp2.set(a2latch.state);
-    //armp3.set(!a2latch.state);
 
-    if (sticks.ButtonL1.pressing()){
-      intake.spin(forward);
-    }else if(sticks.ButtonL2.pressing()){
-      intake.spin(reverse);
-    }else{
-      intake.stop();
-    }
+      lm.spin(forward, fwdVolts + turnVolts, voltageUnits::volt);
+      rm.spin(forward, fwdVolts - turnVolts, voltageUnits::volt);
 
-/*
-    if (arm.position(degrees) < 700 && sticks.ButtonR1.pressing()){
-      arm.spin(forward);
-    }else if (armreset.pressing()){
-      arm.setPosition(0,degrees);
-      arm.stop();
-    }else if(sticks.ButtonR2.pressing()){
-      arm.spin(reverse);
-    }else{
-      arm.stop();
-    }
-  */
-
-    //tlatch.check(sticks.ButtonA.pressing()); // transmission
-    clatch.check(sticks.ButtonX.pressing()); // clamp
-    a1latch.check(sticks.ButtonB.pressing()); // first arm pistons
-    a2latch.check(sticks.ButtonY.pressing()); // 2nd arm pistons
-
-    
+        
     wait(10, msec); 
   }
 }
